55d7ae7f2e8fe8eaeac0e4fb2d7ab5c6d4b2594e
Pickles888 <verysaltypickles@gmail.com>
2024-02-07 08:59:35 -0800

    commit by makefile

--- hello-world!/humans.h
+++ hello-world!/humans.h
@@ -7,12 +7,3 @@ class person {
     double height;
     int brain{0};
 };
-
-void humans() {
-  person Holden;
-  Holden.hands = 2;
-  Holden.feet = 1;
-  Holden.height = .1;
-  std::cout << "Hands: " << Holden.hands << " Feet: " << Holden.feet << " Height: " << Holden.height << " Brain: " << Holden.brain << '\n';
-};
-
